a21a87e4b4bc81829b52192aa237d3640387e6fc
vagrant <vagrant@vagrant.vm>
2018-07-27 19:46:07 +0000

    crud operations program

---
+++ db_crud_operations.py
@@ -0,0 +1,18 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Restaurant, MenuItem
+engine=create_engine("sqlite:///restaurantmenu.db")
+
+Base.metadata.bind=engine
+
+DBSession=sessionmaker(bind=engine)
+
+session=DBSession()
+
+mySecondRestaurant= Restaurant(name="Tiffins")
+session.add(mySecondRestaurant)
+session.commit()
+session.query(Restaurant).all()
+
+for i in session.query(Restaurant).all():
+	print(i.name)
