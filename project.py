e34dc07d9f82ca7866bf0d048a3d67b2645638de
vagrant <vagrant@vagrant.vm>
2018-08-02 03:24:54 +0000

    added edit menu functionality

--- project.py
+++ project.py
@@ -1,32 +1,70 @@
+
 from flask import Flask
-app = Flask(__name__)
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Restaurant, MenuItem
+from flask import render_template, request, redirect, url_for
 
-engine=create_engine("sqlite:///restaurantmenu.db")
+app = Flask(__name__)
+
+engine = create_engine('sqlite:///restaurantmenu.db')
 Base.metadata.bind = engine
 
-DBSession =sessionmaker(bind=engine)
-session= DBSession()
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
 
-@app.route("/")
-@app.route("/hello")
-def HelloWorld():
-    restaurant = session.query(Restaurant).first()
-    items= session.query(MenuItem).filter_by(restaurant_id=restaurant.id)
-    output=""
-    for i in items:
-        output +=i.name
-        output += "</br>"
-        output +=i.price
-        output += "</br>"
-        output +=i.description
-        output += "</br></br>"
+@app.route('/')
+@app.route('/restaurants/<int:restaurant_id>/')
+def restaurantMenu(restaurant_id):
+    restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
+    items = session.query(MenuItem).filter_by(restaurant_id=restaurant.id)
+    return render_template("menu.html", restaurant=restaurant, items=items)
 
-    return output
+@app.route("/restaurant/<int:restaurant_id>/new/")
+def newMenuItem(restaurant_id):
+    return "page to create a new menu item. Task 1 complete!"
 
-if __name__=="__main__":
-    app.debug= True
-    app.run(host="0.0.0.0", port = 5000)
- 
+@app.route("/restaurant/<int:restaurant_id>/<int:menu_id>/edit/")
+def editMenuItem(restaurant_id, menu_id):
+    editedItem=session.query(MenuItem).filter_by(id=menu_id).one()
+    if request.method=="POST":
+        if request.form["name"]:
+            editedItem.name= request.form["name"]
+        session.add(editedItem)
+        session.commit()
+        return redirect(url_for("restaurantMenu", restaurant_id=restaurant_id))
+    else:
+        return render_template("newmenuitem.html", restaurant_id=restaurant_id)
+
+@app.route("/restaurant/<int:restaurant_id>/<int:menu_id>/delete/")
+def deleteMenuItem(restaurant_id, menu_id):
+    return "page to delete a menu item. Task 3 complete!"
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host='0.0.0.0', port=5000)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
